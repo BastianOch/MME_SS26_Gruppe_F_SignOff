8bfabf44fc718d6ed6b7ef4e167d7b5ca70f94bd
StrauTho <t.straubinger94@gmail.com>
2026-08-25 11:28:10 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
+import { Routes, Route } from 'react-router'
 import Sidebar from './components/Sidebar'
 import DashboardPage from './pages/DashboardPage'
+import ProfilePage from './pages/ProfilePage'
+import NewProtocolPage from './pages/NewProtocolPage'
+import ProtocolsPage from './pages/ProtocolsPage'
+import TasksBoardPage from './pages/TasksBoardPage'
+import DocumentHistoryPage from './pages/DocumentHistoryPage'
 
 function App() {
   return (
-    <div className="bg-gray-100 min-h-screen">
+    <div className="flex bg-gray-100 min-h-screen">
       <Sidebar />
-      <DashboardPage />
+
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/profil" element={<ProfilePage />} />
+        <Route path="/protokolle" element={<ProtocolsPage />} />
+        <Route path="/protokolle/neu" element={<NewProtocolPage />} />
+        <Route path="/aufgaben" element={<TasksBoardPage />} />
+        <Route path="/dokumente" element={<DocumentHistoryPage />} />
+      </Routes>
     </div>
   )
 }
