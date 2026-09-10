df4bb26f9d523256d1163f59557631ec64008712
BastianOch <Bastian.Ochsenkuehn@stud.uni-regensburg.de>
2026-09-10 19:16:42 +0200

    Added a "Zurück" button and a basic header allowing the user to start up a new protocoll

--- frontend/src/pages/NewProtocolPage.jsx
+++ frontend/src/pages/NewProtocolPage.jsx
@@ -14,9 +14,20 @@ import {
 function NewProtocolPage() {
   const navigate = useNavigate()
   return (
-    <main>
-      <h1>Neues Meeting - Protokoll</h1>
-      <p>Hier sollen später die Protokolle angelegt werden.</p>
+    // max-w-4xl here is very important to make sure that the form does not stretch too much on larger screens, which would make it harder to read and fill out. The padding ensures that there is enough space around the content, making it more visually appealing and easier to interact with.
+    <main className="flex-1 overflow-y-auto p-6 md:p-10 max-w-4xl">
+      {/* Lets start of with a "zurück" button & the header */}
+      <div classname="mb-6">
+        <Link to="/protokolle" className="flex items-center gap-2 text-gray-500 hover:text-gray-700 transition-colors">
+          <ArrowLeft className="w-4 h-4" />
+          <span>Zurück zur Übersicht</span>
+        </Link>
+        <h1 className="text-2xl font-bold text-gray-900"> Neues Protokoll anlegen</h1>
+        <p className="text-sm text-gray-500 mt-1">
+          Neues Meeting Protokoll anlegen
+        </p>
+      </div>
+
     </main>
   )
 }
